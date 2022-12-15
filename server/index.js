e1778c8888bc7cf723beb79d668525693c99f328
Natalia Fedorova <nataliafedorova1989@gmail.com>
2022-12-15 01:56:27 +0100

    restructure and add server

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+
+app.use(cors());
+
+const port = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/api", (_req, res) => {
+  return res.json({ message: "You have reached the Forum Api" });
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
+
+module.exports.app = app;
