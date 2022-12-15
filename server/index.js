3d23c737a9623fb0ec031f9f8aa59f0353332131
Natalia Fedorova <nataliafedorova1989@gmail.com>
2022-12-15 02:19:24 +0100

    add a post, get posts and get a post

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const Post = require("./models/post");
 
 app.use(cors());
 
@@ -24,6 +25,27 @@ mongoose
     console.log("Mongo connection error!");
     console.log(err);
   });
+
+app.get("/api/posts", async (req, res) => {
+  const posts = await Post.find({}).then((result) => {
+    return res.json(result);
+  });
+});
+
+app.post("/api/posts", async (req, res) => {
+  const post = new Post(req.body);
+  post
+    .save()
+    .then((result) => res.status(201).json(result))
+    .catch((err) => res.json({ error: err.message }));
+});
+
+app.get("/api/posts/:id", async (req, res) => {
+  const { id } = req.params;
+  const post = await Post.findOne({ postId: id }).then((result) => {
+    return res.json(result);
+  });
+});
 
 app.get("/api", (_req, res) => {
   return res.json({ message: "You have reached the Forum Api" });
