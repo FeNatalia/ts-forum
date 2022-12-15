76fc198172f2a1da72dff1e095f4668d71c74b4b
Natalia Fedorova <nataliafedorova1989@gmail.com>
2022-12-15 02:07:28 +0100

    add mongodb

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 app.use(cors());
 
@@ -10,6 +11,19 @@ const port = process.env.PORT || 3001;
 app.use(express.static(path.join(__dirname, "../client/build")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+mongoose
+  .set("strictQuery", true)
+  .connect("mongodb://localhost:27017/todoApp", {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Mongo connection is open!");
+  })
+  .catch((err) => {
+    console.log("Mongo connection error!");
+    console.log(err);
+  });
 
 app.get("/api", (_req, res) => {
   return res.json({ message: "You have reached the Forum Api" });
